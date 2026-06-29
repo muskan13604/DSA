c1be68d48f835232923a0fa5d3edb4a199f4f250
muskan13604 <152528052+muskan13604@users.noreply.github.com>
2026-06-29 16:36:21 +0530

    Time: 65 ms (62.5%), Space: 10.6 MB (87.27%) - LeetHub

---
+++ 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.cpp
@@ -0,0 +1,63 @@
+class Solution {
+public:
+
+    int lcs(int i, int j, int m, int n, string &text1,
+            string &text2) {
+
+        // Space Optimized DP
+        vector<int> prev(n, 0);
+        vector<int> curr(n, 0);
+
+        // Traverse the table
+        for (int i = 0; i < m; i++) {
+
+            // Reset current row
+            curr.assign(n, 0);
+
+            for (int j = 0; j < n; j++) {
+
+                // Characters match
+                if (text1[i] == text2[j]) {
+
+                    int diagonal = 0;
+
+                    if (i > 0 && j > 0)
+                        diagonal = prev[j - 1];
+
+                    curr[j] = 1 + diagonal;
+                }
+                else {
+
+                    int left = 0;
+                    int up = 0;
+
+                    if (j > 0)
+                        left = curr[j - 1];
+
+                    if (i > 0)
+                        up = prev[j];
+
+                    curr[j] = max(left, up);
+                }
+            }
+
+            prev = curr;
+        }
+
+        return prev[n - 1];
+    }
+
+    int longestPalindromeSubseq(string s) {
+
+        string text1 = s;
+
+        reverse(s.begin(), s.end());
+
+        string text2 = s;
+
+        int m = text1.size();
+        int n = text2.size();
+
+        return lcs(m - 1, n - 1, m, n, text1, text2);
+    }
+};
