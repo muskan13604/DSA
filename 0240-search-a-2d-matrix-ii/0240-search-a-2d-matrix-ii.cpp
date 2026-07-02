204eb4b5f194b7a42833fee2d8e75b710ac21180
muskan13604 <152528052+muskan13604@users.noreply.github.com>
2026-07-02 16:40:17 +0530

    Time: 48 ms (78.84%), Space: 18.6 MB (67.69%) - LeetHub

--- 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.cpp
+++ 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.cpp
@@ -7,7 +7,7 @@ public:
         int n = matrix[0].size();
 
         int row = 0;
-        int col = n - 1;   // Start from top-right
+        int col = n - 1;   
 
         while (row < m && col >= 0) {
             if (matrix[row][col] == target) {
