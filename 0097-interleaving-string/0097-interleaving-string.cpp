b65cea89943a92dbefa7aa1153e99bc36f722255
muskan13604 <152528052+muskan13604@users.noreply.github.com>
2026-07-25 14:14:41 +0530

    Time: 0 ms (100%), Space: 8.3 MB (97.64%) - LeetHub

---
+++ 0097-interleaving-string/0097-interleaving-string.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+bool isInterleave(std::string s1, std::string s2, std::string s3) {
+    int m = s1.length();
+    int n = s2.length();
+    int l = s3.length();
+    if (m + n != l) {
+        return false;
+        }
+        std::vector<bool> dp(n + 1, false);
+        dp[0] = true;
+        for (int j = 1; j <= n; ++j) {
+            dp[j] = dp[j - 1] && (s2[j - 1] == s3[j - 1]);
+            }
+            for (int i = 1; i <= m; ++i) {
+                dp[0] = dp[0] && (s1[i - 1] == s3[i - 1]);
+                for (int j = 1; j <= n; ++j) {
+                    dp[j] = (dp[j] && s1[i - 1] == s3[i + j - 1]) ||
+                    (dp[j - 1] && s2[j - 1] == s3[i + j - 1]);
+                    }
+                    }
+                    return dp[n];
+                    }
+                    };
+                
